ae188b15953a5e1215066dc40f2501b696fe681b
pavlovma007 <pavlovma007@gmail.com>
2023-05-08 13:17:05 +0300

    Delete src/_demo/_demo directory

--- src/_demo/_demo/run-1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-qmlscene 	-I ../../../lib/industrial-controls/     -I ../../../build/    ./demo-builded-plugins-1.qml
--- src/_demo/_demo/run-2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-qmlscene 	-I ../../../lib/industrial-controls/     -I ../../../build/    ./demo-builded-plugins-2.qml
